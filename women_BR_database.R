8e79b2f5404955ee003c5c82283820c681a82652
MPCP- UFPE <mpcp@iMac-de-MPCP.local>
2017-11-10 10:18:33 -0300

    second commit

---
+++ women_BR_database.R
@@ -0,0 +1,8 @@
+#==========================================#
+# MAP OF WOMEN DEATH IN BRAZILIAN CITIES   #
+#==========================================#
+# autor: Claudio A. Monteiro               #
+# email: claudiomonteiro@protonmail.com    #
+#------------------------------------------#
+# #usefreesoftware                         #
+#------------------------------------------#
